61e94f2e56e4b3c30db2bcdfd892c6f5239fede3
Sébastien Gortais <sgortais@net-com.fr>
2024-08-04 23:18:35 +0200

    Retrait débug

--- src/app/api/tier/route.ts
+++ src/app/api/tier/route.ts
@@ -49,7 +49,7 @@ export async function GET() {
     bdd.map(w => {
         totalMatch += w.match.length
         returnable.push({
-            name: `${w.name} - ${w.match.filter(m => m.win).length - w.match.filter(m => m.loose).length}pts - ${w.match.length}m`,
+            name: `${w.name}`,
             tier: getTier(w.match),
             pts: w.match.filter(m => m.win).length - w.match.filter(m => m.loose).length,
             matches: w.match.length
