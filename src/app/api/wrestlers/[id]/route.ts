38df6c236ec3c5be0481402446c728550d3a20ab
Sébastien Gortais <sgortais@net-com.fr>
2025-04-25 14:21:46 +0200

    Typage des paramètres

--- src/app/api/wrestlers/[id]/route.ts
+++ src/app/api/wrestlers/[id]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/utils/prisma';
 
+
 export async function PUT(
     request: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
     const myParams = await params;
     try {
