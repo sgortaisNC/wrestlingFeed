a6fd770d280c40091cdf1c52c04394ddff487290
Séb <sgortais@net-com.fr>
2025-08-28 21:38:51 +0200

    Retrait de la parti wiki

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -100,15 +100,6 @@ export const Menu = () => {
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/wiki"} className={isActive('/wiki') ? css.activeLink : ''}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-                                <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
-                            </svg>
-                            Docs
-                        </Link>
-                    </li>
-                    <li>
                         <a href="#" onClick={openModal}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <circle cx="12" cy="12" r="10"></circle>
