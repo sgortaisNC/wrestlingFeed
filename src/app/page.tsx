0ddc2316aafda773a28acfd84509d5bb00af5ee3
sgortaisNC <32925751+sgortaisNC@users.noreply.github.com>
2025-11-14 12:59:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
     const ple = [
     { date: "2025-10-11", event: "WWE Crown Jewel 2025 - Perth" },
     { date: "2025-11-29", event: "Survivor Series 2025 - San Diego, CA" },
-        { date: "2025-11-25", event: "Halloween Havoc 2025" },
+        { date: "2025-10-25", event: "Halloween Havoc 2025" },
     ];
 
 
