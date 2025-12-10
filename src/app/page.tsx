13c286ee5bdf22ebb0485e5b3092265894f11ae8
sgortaisNC <32925751+sgortaisNC@users.noreply.github.com>
2025-12-10 10:11:25 +0100

    PLE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,8 @@ export default async function Home() {
     yesterday.setDate(yesterday.getDate() - 2);
     const shows = [];
     const ple = [
-    { date: "2025-10-11", event: "WWE Crown Jewel 2025 - Perth" },
-    { date: "2025-11-29", event: "Survivor Series 2025 - San Diego, CA" },
-    { date: "2025-11-1", event: "Saturday Night main event" },
-
-        { date: "2025-10-25", event: "Halloween Havoc 2025" },
+    { date: "2025-12-7", event: "NXT Deadline" },
+    { date: "2025-12-14", event: "Saturday Night main event" }
     ];
 
 
